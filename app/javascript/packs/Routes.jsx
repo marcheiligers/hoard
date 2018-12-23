06b25028f1913057949b2483b709c18d0883b898
TinaHeiligers <tina.heiligers@gmail.com>
2018-12-23 15:56:01 -0700

    Begins adding styles, needs to be redone

--- app/javascript/packs/Routes.jsx
+++ app/javascript/packs/Routes.jsx
@@ -4,63 +4,79 @@ import Home from "./home/homeComponent";
 import About from "./about/aboutComponent";
 import Topics from "./topics/topicsComponent";
 
+const appVersion = "0.0.1";
+
 const App = () => (
   <Router>
     <div style={{ display: "flex" }}>
-      <Header />
+      <Header version={appVersion} />
       <Route path="/home" component={Home} />
       <Route path="/about" component={About} />
       <Route path="/topics" component={Topics} />
+      <Route path="/versions/" component={Versions} />
     </div>
   </Router>
 );
 
-const Header = () => (
-  <nav style={{ width: "240px" }}>
-    <ul style={headerStyle}>
-      <li>
-        <NavLink
-          to="/home"
-          activeStyle={{
-            fontWeight: "bold",
-            color: "red"
-          }}
-        >
-          Home
-        </NavLink>
-      </li>
-      <li>
-        <NavLink
-          to="/about"
-          activeStyle={{
-            fontWeight: "bold",
-            color: "red"
-          }}
-        >
-          About
-        </NavLink>
-      </li>
-      <li>
-        <NavLink
-          to="/topics"
-          activeStyle={{
-            fontWeight: "bold",
-            color: "red"
-          }}
-        >
-          Topics
-        </NavLink>
-      </li>
-    </ul>
+const Header = ({ version }) => (
+  <nav className=".j10">
+    <div className="j21 j23 j151 j129 j152 j156">
+      <Hoard v={version} />
+      <hr className="j161" />
+      <ul className="j167 j166">
+        <li>
+          <NavLink
+            to="/home"
+            style={{ lineHeight: "1.5rem", padding: "1rem" }}
+            activeStyle={{
+              fontWeight: "bold",
+              color: "white"
+            }}
+          >
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/about"
+            style={{ lineHeight: "1.5rem", padding: "1rem" }}
+            activeStyle={{
+              fontWeight: "bold",
+              color: "white"
+            }}
+          >
+            About
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/topics"
+            style={{ lineHeight: "1.5rem", padding: "1rem" }}
+            activeStyle={{
+              fontWeight: "bold",
+              color: "white"
+            }}
+          >
+            Topics
+          </NavLink>
+        </li>
+      </ul>
+    </div>
   </nav>
 );
-
-export default App;
-
-const headerStyle = {
-  display: "flex",
-  justifyContent: "space-between",
-  listStyle: "none",
-  margin: "2em",
-  fontFamily: "Helvetica"
+const Hoard = ({ v }) => {
+  return (
+    <div className="j131">
+      <div className="j132">
+        <a className="j117 j118 j130" href="/">
+          <h6 className="j70 j87">Hoard</h6>
+        </a>
+        <a className="j117 j118 j133" href="/versions/">
+          <span className="j70 j80">{`v${v}`}</span>
+        </a>
+      </div>
+    </div>
+  );
 };
+const Versions = () => <div>{appVersion ? appVersion : "v0.0.1"}</div>;
+export default App;
--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -6,15 +6,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import PropTypes from "prop-types";
 import App from "./Routes";
-// const Hello = props => <div>Hello {props.name}!</div>;
-
-// Hello.defaultProps = {
-//   name: "David"
-// };
-
-// Hello.propTypes = {
-//   name: PropTypes.string
-// };
 
 document.addEventListener("DOMContentLoaded", () => {
   ReactDOM.render(
