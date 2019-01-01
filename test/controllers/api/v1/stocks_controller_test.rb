0c11eb754a4a6a3eeadc082cb58641ddb1eeeabf
Marc Heiligers <marc@eternal.co.za>
2019-01-01 15:14:59 -0700

    Fetch additional stock info from IEX API on create with only symbol provided (#9)

--- test/controllers/api/v1/stocks_controller_test.rb
+++ test/controllers/api/v1/stocks_controller_test.rb
@@ -5,18 +5,32 @@ class StocksControllerTest < ActionDispatch::IntegrationTest
     @stock = stocks(:one)
   end
 
-  test "should get index" do
+  test 'should get index' do
     get api_v1_stocks_url
     assert_response :success
   end
 
-  test "should create stock" do
+  test 'should create stock with provided params' do
+    stock_params = { symbol: 'AAPL', name: 'Apple', annual_dividends: 0, heart: false, star: false }
     assert_difference('Stock.count') do
-      post api_v1_stocks_url, params: { stock: { annual_dividends: @stock.annual_dividends, heart: @stock.heart, name: @stock.name, star: @stock.star, symbol: @stock.symbol } }
+      post api_v1_stocks_url, params: { stock: stock_params }
     end
 
     assert_response :created
     assert_response_json_model(Stock.last)
+  end
+
+  test 'should create stock with additional info from API when only symbol is provided' do
+    assert_difference('Stock.count') do
+      VCR.use_cassette('api/v1/stocks/create_symbol') do
+        post api_v1_stocks_url, params: { stock: { symbol: 'AAPL' } }
+      end
+    end
+
+    assert_response :created
+    assert_response_json_model(Stock.last)
+    assert_equal response_json['name'], 'Apple Inc.'
+    assert_equal response_json['annualDividends'], 4
   end
 
   test "should show stock" do
