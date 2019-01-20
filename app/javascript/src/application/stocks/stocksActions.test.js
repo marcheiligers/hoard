be7211c7cc016bc2277ef90b79d625e34cb12d0b
TinaHeiligers <tina.heiligers@gmail.com>
2019-01-20 15:04:31 -0700

    cleans up babel config more, adds tests for redux - WIP

---
+++ app/javascript/src/application/stocks/stocksActions.test.js
@@ -0,0 +1,27 @@
+import data from './stocksData.js';
+import stocksActions from './stocksActions.js';
+
+describe('stocks action creators -> stocksActions', () => {
+  it('gets stocks on loadStocksRequest', () => {
+    const testLoadStocksRequest = stocksActions.loadStocksRequest();
+    expect(testLoadStocksRequest).toEqual({
+      type: stocksActions.LOAD_STOCKS_REQUEST
+    });
+  });
+  it('returns stocks on loadStocksSuccess', () => {
+    const testStocks = data;
+    const testLoadStocksSuccess = stocksActions.loadStocksSuccess(testStocks);
+    expect(testLoadStocksSuccess).toEqual({
+      type: stocksActions.LOAD_STOCKS_SUCCESS,
+      stocks: testStocks
+    });
+  });
+  it('returns and error message on loadStocksError', () => {
+    const testError = { message: 'Could not load stocks' };
+    const testLoadStocksError = stocksActions.loadStocksError(testError);
+    expect(testLoadStocksError).toEqual({
+      type: stocksActions.LOAD_STOCKS_ERROR,
+      error: testError.message
+    });
+  });
+});
---
+++ app/javascript/src/application/stocks/stocksReducer.test.js
@@ -0,0 +1,26 @@
+import stocksReducer, { initialState } from './stocksReducer.js';
+import stocksActions from './stocksActions.js';
+
+describe('stocks reducer -> load stocks', () => {
+  const defaultState = stocksReducer(initialState, { type: 'unexpected' });
+  it('returns an object', () => {
+    expect(defaultState).toBeInstanceOf(Object);
+  });
+  it('returns an object equal to initialState', () => {
+    const expected = ['allStocks', 'selectedStock', 'error'];
+    expect(Object.keys(defaultState)).toEqual(expect.arrayContaining(expected));
+  });
+  it('updates state on LOAD_STOCKS_SUCCESS', () => {
+    const testStocks = [];
+    let testAction = stocksActions.loadStocksSuccess(testStocks);
+    const newState = stocksReducer(defaultState, testAction);
+    expect(newState.allStocks).toEqual(testStocks);
+    expect(newState.error).toBeNull();
+  });
+  it('updates state on LOAD_STOCKS_ERROR', () => {
+    const testError = { message: 'Could not load stocks' };
+    let testAction = stocksActions.loadStocksError(testError);
+    const newState = stocksReducer(defaultState, testAction);
+    expect(newState.error).toEqual(testError.message);
+  });
+});
