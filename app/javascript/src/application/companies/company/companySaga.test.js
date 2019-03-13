09275c781d7c0e505b8b989e2e1d13dbfe98dc6b
TinaHeiligers <tina.heiligers@gmail.com>
2019-03-13 16:13:21 -0700

    Adds saga tests for chart data

--- app/javascript/src/application/companies/company/companySaga.test.js
+++ app/javascript/src/application/companies/company/companySaga.test.js
@@ -1,9 +1,11 @@
 import { call, put, takeEvery, select } from 'redux-saga/effects';
-import { loadCompany } from './companyServices';
+import { loadCompany, loadCompanyChartData } from './companyServices';
 import companyActions from './companyActions';
 import {
   loadCompanyRequestWatcher,
-  loadCompanyRequest
+  loadCompanyRequest,
+  loadCompanyChartDataRequestWatcher,
+  loadCompanyChartDataRequest
 } from './companySaga';
 import companyData from './companyData';
 
@@ -36,6 +38,38 @@ describe('stocks saga -> loadCompanyRequest', () => {
     const testError = { message: 'Could not load company' };
     expect(loadCompanyRequestGen.throw(testError).value).toEqual(
       put({ type: companyActions.LOAD_COMPANY_ERROR, error: 'Could not load company' })
+    );
+  });
+});
+describe('stocks saga -> loadCompanyChartDataRequestWatcher', () => {
+  const loadCompanyChartDataRequestWatcherGen = loadCompanyChartDataRequestWatcher();
+  it('should act on every LOAD_COMPANY_CHART_DATA_REQUEST action', () => {
+    expect(loadCompanyChartDataRequestWatcherGen.next().value).toEqual(
+      takeEvery(companyActions.LOAD_COMPANY_CHART_DATA_REQUEST, loadCompanyChartDataRequest)
+    );
+  });
+});
+describe('stocks saga -> loadCompanyChartDataRequest', () => {
+  const testAction = { symbol: companyData.companies[0].symbol, dateRange: '1d' };
+  const loadCompanyChartDataRequestGen = loadCompanyChartDataRequest(testAction);
+  it('should call the api', () => {
+    expect(loadCompanyChartDataRequestGen.next().value).toEqual(
+      call(loadCompanyChartData, testAction.symbol, testAction.dateRange)
+    );
+  });
+  it('should put LOAD_COMPANY_CHART_DATA_SUCCESS on success of the api call', () => {
+    const testResult = { data: companyData.stockDataForOneDay };
+    expect(loadCompanyChartDataRequestGen.next(testResult).value).toEqual(
+      put({
+        type: companyActions.LOAD_COMPANY_CHART_DATA_SUCCESS,
+        chartData: testResult.data
+      })
+    );
+  });
+  it('should put LOAD_COMPANY_CHART_DATA_ERROR on an error', () => {
+    const testError = { message: 'Could not fetch chart data' };
+    expect(loadCompanyChartDataRequestGen.throw(testError).value).toEqual(
+      put({ type: companyActions.LOAD_COMPANY_CHART_DATA_ERROR, error: 'Could not fetch chart data' })
     );
   });
 });
